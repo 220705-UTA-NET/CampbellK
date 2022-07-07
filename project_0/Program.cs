ba1e503e3360f15c05e2ae630d756192c112fb6d
CampbellKT95 <campbellkt2013@gmail.com>
2022-07-07 17:58:04 -0400

    moved db connectivity to a seperate file/namespace

--- project_0/Program.cs
+++ project_0/Program.cs
@@ -1,86 +1,14 @@
 ﻿using System;
-using Npgsql;
+using database;
 
-namespace database
+namespace project_0
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            dbConnection.dbConnect();
-        }
-    }
-    
-    class dbConnection
-    {
-        private static string Host = System.Environment.GetEnvironmentVariable("postgres_host");
-        private static string Username = System.Environment.GetEnvironmentVariable("postgres_username");
-        private static string Database = System.Environment.GetEnvironmentVariable("postgres_database");
-        private static string Port = System.Environment.GetEnvironmentVariable("postgres_port");
-        private static string Password = System.Environment.GetEnvironmentVariable("postgres_password");
-        static public void dbConnect()
-        {
-            Console.WriteLine("Connecting to postgresql...");
-
-            string connectionString = String.Format(
-                "server={0};Username={1};Database={2};Port={3};Password={4}",
-                Host,
-                Username,
-                Database,
-                Port,
-                Password
-            );
-
-            // establishing connection to postgresql
-            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
-
-            // open connection to db, enabling execution of queries
-            conn.Open();
-
-            // check what the user wants to do
-            // ** how to allow the user to continuously revisit? **
-            // constantly utilize Console.ReadLine() at the end of each endpoint
-            string userCommand = Console.ReadLine();
-
-            // will want to set up a prepared statement
-            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM budget", conn);
-
-            // execute query and save results 
-            NpgsqlDataReader reader = command.ExecuteReader();
-
-            // list where table data will be saved
-            List<Dictionary<string, string>> listOfEntries = new List<Dictionary<string, string>>();
-
-            while (reader.Read())
-            {
-                string id = reader["id"].ToString();
-                string description = reader["description"].ToString();
-                string amount = reader["amount"].ToString();
-                string category = reader["category"].ToString();
-                string date = reader["date"].ToString();
-
-                // combine the row data into a single struct to save to listOfEntries;
-                Dictionary<string, string> budgetEntry = new Dictionary<string, string>()
-                {
-                    {"id", id},
-                    {"description", description},
-                    {"amount", amount},
-                    {"category", category},
-                    {"date", date}
-                };
-
-                listOfEntries.Add(budgetEntry);
-            }
-
-            Console.WriteLine(listOfEntries[0]["id"]);
-
-            // end the reader
-            reader.Close();
-            // discard the command
-            command.Dispose();
-            // close the db connection
-            conn.Close();
+            DbConnection conn = new DbConnection();
+            conn.DbConnect();
         }
     }
 }
